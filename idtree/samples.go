e5b16dec37f7732352e989f5c7fd72395ace718a
Alex Nichol <unixpickle@gmail.com>
2016-05-02 20:49:28 -0400

    drafted new idtree implementation
    This implementation is (or rather, will be) much more robust than the previous one, since it was designed for this specific application. No more string hacking or unneeded memory wasting.

---
+++ idtree/samples.go
@@ -0,0 +1,64 @@
+package whichlang
+
+import "github.com/unixpickle/whichlang/tokens"
+
+type linearSample struct {
+	freqs []float64
+	lang  string
+}
+
+func freqsToLinearSamples(toks []string, freqs map[string][]tokens.Freqs) []linearSample {
+	res := make([]linearSample, 0)
+	for lang, freqsList := range freqs {
+		for _, freqs := range freqsList {
+			s := linearSample{
+				lang:  lang,
+				freqs: make([]float64, len(toks)),
+			}
+			for i, tok := range toks {
+				s.freqs[i] = freqs[tok]
+			}
+		}
+	}
+	return res
+}
+
+func languageMajority(samples []linearSample) string {
+	counts := map[string]int{}
+	for _, sample := range samples {
+		counts[sample.lang]++
+	}
+
+	var maxCount int
+	var maxLang string
+	for lang, count := range counts {
+		if count > maxCount {
+			maxCount = count
+			maxLang = lang
+		}
+	}
+
+	return maxLang
+}
+
+// A sampleSorter implements sort.Interface
+// and facilitates sorting linear samples
+// by the frequency of a given token.
+type sampleSorter struct {
+	samples  []linearSample
+	tokenIdx int
+}
+
+func (s *sampleSorter) Len() int {
+	return len(s.samples)
+}
+
+func (s *sampleSorter) Swap(i, j int) {
+	s.samples[i], s.samples[j] = s.samples[j], s.samples[i]
+}
+
+func (s *sampleSorter) Less(i, j int) bool {
+	f1 := s.samples[i].freqs[s.tokenIdx]
+	f2 := s.samples[j].freqs[s.tokenIdx]
+	return f1 < f2
+}
--- idtree/train.go
+++ idtree/train.go
@@ -1,95 +1,206 @@
 package idtree
 
 import (
-	"strconv"
-	"strings"
+	"math"
+	"runtime"
+	"sort"
 
-	"github.com/unixpickle/weakai/idtrees"
 	"github.com/unixpickle/whichlang/tokens"
 )
+
+type splitInfo struct {
+	TokenIdx  int
+	Threshold float64
+	Entropy   float64
+}
 
 // Train returns a *Classifier which is the
 // result of running ID3 on a set of training
 // samples.
 func Train(freqs map[string][]tokens.Freqs) *Classifier {
-	allWords := map[string]bool{}
-	entryCount := 0
-	for _, samples := range freqs {
-		for _, sample := range samples {
-			for word := range sample {
-				allWords[word] = true
-			}
-			entryCount++
-		}
-	}
-
-	dataSet := &idtrees.DataSet{
-		Entries: make([]idtrees.Entry, 0, entryCount),
-		Fields:  make([]idtrees.Field, 0),
-	}
-
-	for lang, list := range freqs {
-		for _, wordMap := range list {
-			entry := &treeEntry{
-				language:    lang,
-				freqs:       wordMap,
-				fieldValues: []idtrees.Value{},
-			}
-			dataSet.Entries = append(dataSet.Entries, entry)
-		}
-	}
-
-	for word := range allWords {
-		idtrees.CreateBisectingFloatFields(dataSet, func(e idtrees.Entry) float64 {
-			return e.(*treeEntry).freqs[word]
-		}, func(e idtrees.Entry, v idtrees.Value) {
-			te := e.(*treeEntry)
-			te.fieldValues = append(te.fieldValues, v)
-		}, strings.Replace(word, "%", "%%", -1)+" > %f")
-	}
-
-	tree := idtrees.GenerateTree(dataSet)
-	res := convertTree(tree)
-	centerThresholdsRoot(res, freqs)
-
-	return res
+	toks := allTokens(freqs)
+	samples := freqsToLinearSamples(toks, freqs)
+	return generateClassifier(toks, samples)
 }
 
-func convertTree(t *idtrees.TreeNode) *Classifier {
-	if t.BranchField == nil {
-		if t.LeafValue == nil {
-			lang := "Unknown"
-			return &Classifier{LeafClassification: &lang}
-		} else {
-			lang := t.LeafValue.String()
-			return &Classifier{LeafClassification: &lang}
+func allTokens(freqs map[string][]tokens.Freqs) []string {
+	words := make([]string, 0)
+	seenWords := map[string]bool{}
+	for _, freqsList := range freqs {
+		for _, freqs := range freqsList {
+			for word := range freqs {
+				if !seenWords[word] {
+					seenWords[word] = true
+					words = append(words, word)
+				}
+			}
 		}
 	}
+	return words
+}
 
-	comps := strings.Split(t.BranchField.String(), " ")
-	if len(comps) != 3 {
-		panic("unknown branch field: " + t.BranchField.String())
+// generateClassifier generates a classifier
+// for the given set of samples.
+func generateClassifier(toks []string, s []linearSample) *Classifier {
+	tokIdx, thresh := bestDecision(s)
+	if tokIdx == -1 {
+		lang := languageMajority(s)
+		return &Classifier{
+			LeafClassification: &lang,
+		}
 	}
-	val, _ := strconv.ParseFloat(comps[2], 64)
 	res := &Classifier{
-		Keyword:   comps[0],
-		Threshold: val,
+		Keyword:   toks[tokIdx],
+		Threshold: thresh,
 	}
-	res.FalseBranch = convertTree(t.Branches[idtrees.BoolValue(false)])
-	res.TrueBranch = convertTree(t.Branches[idtrees.BoolValue(true)])
+	f, t := splitData(s, tokIdx, thresh)
+	res.FalseBranch = generateClassifier(toks, f)
+	res.TrueBranch = generateClassifier(toks, t)
 	return res
 }
 
-type treeEntry struct {
-	language    string
-	freqs       tokens.Freqs
-	fieldValues []idtrees.Value
+func splitData(s []linearSample, tokIdx int, thresh float64) (f, t []linearSample) {
+	f = make([]linearSample, 0, len(s))
+	t = make([]linearSample, 0, len(s))
+
+	for _, sample := range s {
+		if sample.freqs[tokIdx] > thresh {
+			t = append(t, sample)
+		} else {
+			f = append(f, sample)
+		}
+	}
+
+	return
 }
 
-func (t *treeEntry) FieldValues() []idtrees.Value {
-	return t.fieldValues
+// bestDecision returns the token and threshold
+// which split the samples optimally (by the
+// criterion of entropy).
+// If no split exists, this returns (-1, -1).
+func bestDecision(s []linearSample) (tokIdx int, thresh float64) {
+	maxProcs := runtime.GOMAXPROCS(0)
+
+	toksPerGo := len(toks) / maxProcs
+	splitChan := make(chan *splitInfo, maxProcs)
+	for i := 0; i < maxProcs; i++ {
+		tokCount := toksPerGo
+		tokStart := toksPerGo * i
+
+		// The last set might need to be slightly larger
+		// due to division truncation.
+		if i == maxProcs-1 {
+			tokCount = len(toks) - tokStart
+		}
+
+		go bestNodeSubset(tokStart, tokCount, s, splitChan)
+	}
+
+	var best *splitInfo
+	for i := 0; i < maxProcs; i++ {
+		res := <-splitChan
+		if res == nil {
+			continue
+		}
+		if best == nil || res.Entropy < best.Entropy {
+			best = res
+		}
+	}
+
+	if best == nil {
+		return -1, -1
+	}
+
+	return best.TokenIdx, best.Threshold
 }
 
-func (t *treeEntry) Class() idtrees.Value {
-	return idtrees.StringValue(t.language)
+func bestNodeSubset(startIdx, count int, s []linearSample, res chan<- *splitInfo) {
+	bestThresh := -1.0
+	var bestEntropy float64
+	var bestIdx int
+	for i := 0; i < count; i++ {
+		idx := startIdx + i
+		thresh, entropy := bestSplit(s, idx)
+		if thresh < 0 {
+			continue
+		} else if bestThresh < 0 || entropy < bestEntropy {
+			bestEntropy = entropy
+			bestThresh = thresh
+			bestIdx = idx
+		}
+	}
+	if bestThresh == -1 {
+		res <- nil
+	} else {
+		res <- &splitInfo{bestIdx, bestThresh, bestEntropy}
+	}
+}
+
+// bestSplit finds the ideal threshold for splitting
+// samples by a given token (specified by an index).
+// This returns the threshold and the resulting entropy.
+// The threshold will be -1 if no split is useful.
+func bestSplit(s []linearSample, tokenIdx int) (thres float64, entrop float64) {
+	sortedArray := make([]linearSample, len(s))
+	copy(sortedArray, s)
+	sorter := &sampleSorter{sortedGroup, tokenIdx}
+	sort.Sort(sorter)
+
+	lowerDistribution := map[string]int{}
+	upperDistribution := map[string]int{}
+
+	for _, sample := range sortedArray {
+		upperDistribution[sample.lang]++
+	}
+
+	if len(upperDistribution) == 1 {
+		// Can't split homogeneous data effectively.
+		return -1, -1
+	}
+
+	thresh = -1
+	entrop = -1
+
+	if len(sortedArray) == 0 {
+		return
+	}
+
+	lastFreq := sortedArray[0].freqs[tokenIdx]
+	for i := 1; i < len(s); i++ {
+		upperDistribution[s[i-1].freqs[tokenIdx]]--
+		lowerDistribution[s[i-1].freqs[tokenIdx]]++
+
+		freq := s[i].freqs[tokenIdx]
+		if freq == lastFreq {
+			continue
+		}
+
+		upperFrac := float64(len(s)-i) / float64(len(s))
+		lowerFrac := float64(i) / float64(len(s))
+		disorder := upperFrac*distributionEntropy(upperDistribution) +
+			lowerFrac*distributionEntropy(lowerDistribution)
+		if disorder < entrop || thresh == -1 {
+			entrop = disorder
+			thresh = (lastFreq + freq) / 2
+		}
+
+		lastFreq = freq
+	}
+
+	return
+}
+
+func distributionEntropy(dist map[string]int) float64 {
+	var res float64
+	var totalCount int
+	for _, count := range dist {
+		totalCount += count
+	}
+	for _, count := range dist {
+		fraction := float64(count) / float64(totalCount)
+		if fraction != 0 {
+			res -= math.Log(fraction) * fraction
+		}
+	}
+	return res
 }
