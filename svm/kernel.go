de81431ddc0d4ae355397955abcfb8a36e2bbe83
Alex Nichol <unixpickle@gmail.com>
2016-05-06 17:52:53 -0400

    added Kernel.String() routine
    This will be nice for verbose trainer output.

--- svm/kernel.go
+++ svm/kernel.go
@@ -1,6 +1,7 @@
 package svm
 
 import (
+	"fmt"
 	"math"
 	"strconv"
 
@@ -30,6 +31,8 @@ type Kernel struct {
 	Params []float64
 }
 
+// Product returns the product of two vectors
+// under this kernel.
 func (k *Kernel) Product(v1, v2 linalg.Vector) float64 {
 	switch k.Type {
 	case LinearKernel:
@@ -45,6 +48,27 @@ func (k *Kernel) Product(v1, v2 linalg.Vector) float64 {
 		}
 		diff := v1.Copy().Scale(-1).Add(v2)
 		return math.Exp(-k.Params[0] * diff.Dot(diff))
+	default:
+		panic("unknown kernel type: " + strconv.Itoa(int(k.Type)))
+	}
+}
+
+// String returns a mathematical formula which
+// represents this kernel (e.g. "(x*y+1)^2").
+func (k *Kernel) String() string {
+	switch k.Type {
+	case LinearKernel:
+		return "x*y"
+	case PolynomialKernel:
+		if len(k.Params) != 2 {
+			panic("expected two parameters for polynomial kernel")
+		}
+		return fmt.Sprintf("(x*y + %f)^%f", k.Params[0], k.Params[1])
+	case RadialBasisKernel:
+		if len(k.Params) != 1 {
+			panic("expected one parameter for radial basis kernel")
+		}
+		return fmt.Sprintf("exp(-%f*(x*y)^2)", k.Params[0])
 	default:
 		panic("unknown kernel type: " + strconv.Itoa(int(k.Type)))
 	}
